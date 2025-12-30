ea319629777d1bbb0869da2825e58a02b6429408
Howon Lee <kck4156@kagamine-rin.com>
2025-12-30 16:00:01 +0900

    Revert "Improve proxy header handling and HTTP compliance"
    This reverts commit 8131eed3d23116ef7b72d6ab520a27e5a3fb2d4f.

--- CorsProxy/Program.cs
+++ CorsProxy/Program.cs
@@ -4,14 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
-// Configure a named client for proxying so we can control handler behaviour
-builder.Services.AddHttpClient("proxy").ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-{
-    AllowAutoRedirect = false,
-    UseCookies = false,
-    AutomaticDecompression = System.Net.DecompressionMethods.None
-});
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
