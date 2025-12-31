114fe8c3df53097e8eb19b98878ca30b20e91abd
Howon Lee <kck4156@kagamine-rin.com>
2025-12-31 13:48:33 +0900

    bugfix

--- CorsProxy/Program.cs
+++ CorsProxy/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
 
